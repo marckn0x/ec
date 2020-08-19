81d6fbe1852d3b20cffa651e6062dd1aca146018
Marc Burns <m4burns@uwaterloo.ca>
2020-08-19 13:11:34 -0400

    Fix dependency issues

--- info.rkt
+++ info.rkt
@@ -1,8 +1,12 @@
 #lang setup/infotab
-(define version "0.0.1")
+(define version "0.0.2")
 (define collection 'multi)
-(define deps '("base" "math-lib" "binaryio"))
+(define deps '("base"
+               "math-lib"
+               "binaryio"
+               "typed-racket-lib"))
 (define build-deps '("racket-doc"
                      "rackunit-lib"
                      "scribble-lib"
-                     "crypto-lib"))
+                     "crypto-lib"
+                     "rackunit-typed"))
